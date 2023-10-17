8b83ce169b9fe8388718bead3e7acd25a832fc52
YQisme <1398757912@qq.com>
2023-10-17 18:00:34 +0800

    汉化设置

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "概率论与数理统计——笔记",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
